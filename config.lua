49ca5fca98c8bc3c72325220b895b60ebcb612bb
dufuspaelli <me@dimits.net>
2022-03-24 18:13:09 +0200

    Revert "config update"
    This reverts commit c51dcd99a2c550abbb8aeaec8f0c088aac8649f1.

--- config.lua
+++ config.lua
@@ -13,7 +13,3 @@ Config.PedCoords = vector4(-67.08,-802.96,243.43,158.41) -- Create preview ped a
 Config.HiddenCoords = vector4(-80.23, -805.54, 243.39, 156.5) -- Hides your actual ped while you are in selection
 Config.CamCoords = vector4(-67.7,-804.58,243.84,340.33) -- Camera coordinates for character preview screen ]]
 
-Config.DefaultNumberOfCharacters = 5 -- Define maximum amount of default characters (maximum 5 characters defined by default)
-Config.PlayersNumberOfCharacters = { -- Define maximum amount of player characters by rockstar license (you can find this license in your server's database in the player table)
-    { license = "license:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", numberOfChars = 2 },
-}
